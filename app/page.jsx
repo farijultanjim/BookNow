246b0549ebc4f142375cee5b8ce78ea3b4543970
Farijul Tanzil <farijul35-2888@diu.edu.bd>
2024-10-25 18:03:18 +0600

    screen height adjusted

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-[url('/bg.jpg')] bg-cover bg-center flex items-center justify-center p-5">
-      <div >
+    <div className="h-screen ">
+      <div className="bg-[url('/bg.jpg')] bg-cover bg-center flex items-center justify-center p-10 min-h-screen">
         <FlightSearchHero />
       </div>
     </div>
